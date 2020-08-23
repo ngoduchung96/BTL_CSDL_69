6ee42ab1fd0e805ca89ef797cfb904289411bb44
ngoduchung96 <hungnd@reactplus.com>
2020-08-24 00:12:19 +0700

    chang folder location

--- script.js
+++ script.js
@@ -12,10 +12,12 @@ const loadLibrary = async () => {
     await faceapi.loadFaceRecognitionModel(MODEL_URL)
 }
 
-function start() {
+async function start() {
     const container = document.createElement('div') // tạo 1 thẻ div trống 
     container.style.position = 'relative'
     document.body.append(container) // đặt thẻ container ngay dưới thẻ input và trên dòng loaded
+    const labeledFaceDescriptors = await loadLabelImage()
+    const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6)
     document.body.append('loaded')
     imageUpload.addEventListener('change',async () => {
         const image = await faceapi.bufferToImage(imageUpload.files[0]) // chuyển từ dang buffer về dạng hình ảnh
@@ -24,24 +26,29 @@ function start() {
         container.append(canvas)
         const displaySize = {width:image.width, height:image.height}
         // faceapi.matchDimensions(canvas, displaySize) // cho tranh sơn dầu có cùng kích thước với ảnh để detect
-        const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors() // phát hiện tất cả các khuôn mặt trong tấm ảnh
+        const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptor() // phát hiện tất cả các khuôn mặt trong tấm ảnh
         console.log('detections.length',detections.length) // log số khuôn mặt trong hình
         const resizeDetections = faceapi.resizeResults(detections, displaySize)
-        resizeDetections.forEach(detection => {
-            const box = detection.detection.box
-            const drawBox = new faceapi.draw.DrawBox(box, {label: 'face'})
+        const results = resizeDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
+        results.forEach((result,index) => {
+            const box = resizeDetections[index].detection.box
+            const drawBox = new faceapi.draw.DrawBox(box, {label: result.toString()})
             drawBox.draw(canvas)
         }) 
     })
 }
 
 function loadLabelImage() {
-    const labels = ['Hung']
+    const labels = ['hung']
     return Promise.all(
         labels.map(async label => {
-            for(let i = 1;i<=3;i++) {
-                const img = faceapi.fetchImages(``)
+            const descriptions = []
+            for(let i = 1;i<=2;i++) {
+                const img = await faceapi.fetchImage(`https://github.com/ngoduchung96/BTL_CSDL_69/tree/master/images/${label}/${i}.jpg`)
+                const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptors()
+                descriptions.push(detections.descriptor)
             }
+            return new faceapi.labeledFaceDescriptors(label, descriptions)
         })
     )
 }
